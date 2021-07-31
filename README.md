2953cbd4dba1a69a0e12448574c1eaa12328cbd4
Noureddin <noureddin95@gmail.com>
2021-07-31 17:24:27 +0200

    add text send feature; make design more responsive

--- README.md
+++ README.md
@@ -3,15 +3,16 @@
 Flask-based LAN communication tool, to be run by a technical user but used by non-technical users on the same nework.
 
 - Simple, minimal UI, very easy for non-technical users.
+- Responsive design, viewable on desktop and mobile.
 - Easily localizable and customizable.
 
 ## Functions
 
 - Send a beep (just a notification).
+- Send a text message.
 
 ### To-do
 
-- Send a text message.
 - Send a voice message.
 - Initiate a voice call.
 
--- lanbeeper.py
+++ lanbeeper.py
@@ -3,22 +3,29 @@
 # based on https://stackoverflow.com/q/19794695
 
 from flask import Flask, render_template, request
-
 import subprocess
-def beep():
+
+def render(**kw):
+    return render_template('lanbeeper.html', **uitxt, **kw)
+
+def notify(msg=None):
     subprocess.call([
         b'notify-send',
         b'-u', b'critical',
         b'LanBeeper',
-        b'X' * 140
+        b'X' * 140 if msg is None else msg.encode('utf-8')
     ])
 
 uitxt = {
     'html_dir': 'ltr',
     'html_lang': 'en',
+    'head_msg': '', # 'Welcome to LanBeeper!',
     'send_beep': 'Send Beep!',
+    'send_text': 'Send Text!',
+    'send_text_lbl': 'Or, send a text message instead:',
 }
 beep_sent_msg = 'Beep Sent!'
+text_sent_msg = 'Text Sent!'
 
 app = Flask(__name__)
 
@@ -26,9 +33,12 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         if 'beep' in request.form:
-            beep()
-            return render_template('lanbeeper.html', **uitxt, msg=beep_sent_msg)
-    return render_template('lanbeeper.html', **uitxt)
+            notify()
+            return render(msg=beep_sent_msg)
+        if 'text' in request.form:
+            notify(request.form['textarea'])
+            return render(msg=text_sent_msg)
+    return render()
 
 
 if __name__ == '__main__':
